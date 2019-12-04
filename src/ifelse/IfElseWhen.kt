2b8ca8b46e203f129ae996bc6151d6238e653e61
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-12-04 21:00:09 +0300

    Operators If - Else When

---
+++ src/ifelse/IfElseWhen.kt
@@ -0,0 +1,68 @@
+/*
+ *
+ *   Created by Arda Kazancı on 4.12.2019 20:44
+ *   Copyright (c) 2019 . All rights reserved.
+ *   Last modified 4.12.2019 20:44
+ *
+ */
+
+package ifelse
+
+fun main(args: Array<String>) {
+
+    // Exercise 1
+
+    var sayi = 100
+    if (sayi < 1000) {
+        println("sayı 1000 den küçük")
+    } else {
+        println("sayı 1000 den büyük")
+    }
+
+    // Exercise 2
+    var sicaklik = 10
+    if (sicaklik >= 30 && sicaklik >= 0) {
+        println("30 dan büyük")
+    } else {
+        println("30 dan Küçük")
+    }
+
+    // Exercise 3
+    var isLogin = false
+    if (isLogin)
+        println("Giriş Başarılı")
+    else
+        println("Giriş Başarısız")
+
+    // Exercise 4
+    var sayi_2 = 7
+    when (sayi_2) {
+        1 -> {
+            println("Sayı 1")
+        }
+        2 -> {
+            println("Sayı 2")
+        }
+        3 -> println("Sayı 3")
+
+        else -> println("Sayı 4")
+    }
+
+    // Exercise 5
+    var sayi_3 = 10
+    var sonuc = when (sayi_3) {
+        0, 1 -> "Sıfır Bir"
+        2, 3 -> "İki Üç"
+        else -> "Bilinmeyen sayılar olabilir."
+    }
+
+
+    // Exercise 6
+    var sayi_5 = 30
+    when (sayi_5) {
+        in 1..30 -> println("Girilen Sayı 1 ile 30 arasındadır.")
+        !in 1..30 -> println("Girilen Sayı 1 ile 30 arasında değildir.")
+    }
+
+
+}
--- src/operators/Operators.kt
+++ src/operators/Operators.kt
@@ -12,13 +12,71 @@ package operators
 /*
     Operatörler
     ------------
-    -> Atama Operatörleri
+    -> Atama Operatörleri -> Sağdan Sola
     -> Artimetik Operatörler
     -> İlişkisel Operatörler
     -> Mantıksal Operatörler
     -> Birleştirme Operatörleri
  */
 
-fun main(args : Array<String>){
+fun main(args: Array<String>) {
+
+    // Aritmetik Operatörler
+    /*
+    var x = 5
+
+    println(4 + 5)
+    println(4 - 5)
+    println(4 * 5)
+    println(4 / 5) // Ondalık
+    println(4 % 5) // Ondalık
+    println("----------------")
+    println(x++)
+    println(x)
+    println("----------------")
+    println(x--)
+    println(x)
+    println("----------------")
+    println(++x)
+    println(x)
+    println("----------------")
+    println(--x)
+    println(x)
+    println("----------------")
+    x = x + 2
+    x += 2
+    println(x)
+    println("----------------")
+    */
+
+    // İlişkisel Operatörler
+    /*
+    var sayi_1 = 10
+    var sayi_2 = 20
+
+    println(sayi_1 == sayi_2)
+    println(sayi_1 >= sayi_2)
+    println(sayi_1 <= sayi_2)
+    println(sayi_1 < sayi_2)
+    println(sayi_1 > sayi_2)
+    println(sayi_1 != sayi_2) // Eşit değilse.
+    */
+
+    // Mantıksal Operatörler
+    /*
+    var a = true
+    var b = false
+    println(a && b)
+    println(a || b)
+     */
+
+    // Birleştirme Operatörü
+    /*
+    var a = "Ayça"
+    var b = "Arda"
+    var c = a + b
+    println(c)
+     */
+
 
 }
